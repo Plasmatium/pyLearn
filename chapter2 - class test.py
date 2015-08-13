4ddf10cf2d52223b05f544cb0675391beb2cc3bc
Jonny <slv309@163.com>
2015-08-13 15:04:57 +0800

    chapter2 - class test

---
+++ chapter2 - class test.py
@@ -0,0 +1,15 @@
+# sicp chapter2 ---- class test
+# 15-8-13
+
+class Account(object):
+        def __init__(self, account_holder):
+            self.balance = 0
+            self.holder = account_holder
+        def deposit(self, amount):
+            self.balance = self.balance + amount
+            return self.balance
+        def withdraw(self, amount):
+            if amount > self.balance:
+                return 'Insufficient funds'
+            self.balance = self.balance - amount
+            return self.balance
