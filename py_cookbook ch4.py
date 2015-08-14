cc6f7ef94fd0c02808dacad5ca0b00ded37ce08f
Jonny <slv309@163.com>
2015-08-14 17:11:09 +0800

    make_rand_node
    not complete

--- py_cookbook ch4.py
+++ py_cookbook ch4.py
@@ -1,4 +1,5 @@
 # python cookbook chapter 4
+from random import random
 
 def frange(start, stop, increment):
     x = start
@@ -16,6 +17,7 @@ class Node:
 
     def add_child(self, node):
         self._children.append(node)
+        return self._children[-1]
 
     def __iter__(self):
         return iter(self._children)
@@ -25,3 +27,14 @@ class Node:
         for c in self:
             yield from c.depth_first()
             
+def make_rand_node(r):
+	curr = Node(-1)
+	li = [curr]
+	for tag in r:
+		if tag == 1:
+			li.append(curr)
+			curr = curr.add_child(Node(str(tag)))
+		else:
+			curr = li.pop()
+	return curr
+
