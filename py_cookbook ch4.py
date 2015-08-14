96f9a1aee6b1051598ad3ce2e0bb3c1821164674
Plasmatium <slv309@163.com>
2015-08-14 23:23:51 +0800

    update
    make_rand_node -> complete

--- py_cookbook ch4.py
+++ py_cookbook ch4.py
@@ -1,4 +1,4 @@
-# python cookbook chapter 4
+﻿# python cookbook chapter 4
 from random import random
 
 def frange(start, stop, increment):
@@ -7,7 +7,7 @@ def frange(start, stop, increment):
         yield x
         x += increment
 
-class Node:
+class Node(object):
     def __init__(self, value):
         self._value = value
         self._children = []
@@ -27,14 +27,23 @@ class Node:
         for c in self:
             yield from c.depth_first()
             
-def make_rand_node(r):
-	curr = Node(-1)
-	li = [curr]
-	for tag in r:
-		if tag == 1:
-			li.append(curr)
-			curr = curr.add_child(Node(str(tag)))
-		else:
-			curr = li.pop()
-	return curr
+def make_rand_node(root, level):
+    if level == 0:
+        return
+    curr = root
+    for i in range(int(random()*level)):
+        curr.add_child(Node('level{0}.{1}'.format(level, i)))
+    for cld in curr:
+        make_rand_node(cld, level-1)
+
+#use yield & generator to read file within specified block size
+def read_file(fpath): 
+   BLOCK_SIZE = 1024 
+   with open(fpath, 'rb') as f: 
+       while True: 
+           block = f.read(BLOCK_SIZE) 
+           if block: 
+               yield block 
+           else: 
+               return
 
